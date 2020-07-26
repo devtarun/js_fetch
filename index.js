b2208a95d8e66fd1ac4c27fbb9a4031abc6c1870
TARUN <tarun.kist@gmail.com>
2020-07-26 17:00:29 +0530

    Final commit

--- index.js
+++ index.js
@@ -1,60 +1,92 @@
-// const fetch = require('node-fetch');
-
 const FILE_URL = "http://norvig.com/big.txt";
 const API_URL = "https://dictionary.yandex.net/api/v1/dicservice.json/lookup";
 const API_KEY = "dict.1.1.20170610T055246Z.0f11bdc42e7b693a.eefbde961e10106a4efa7d852287caa49ecc68cf";
 const skippedWords = ["into", "there", "were", "although", "even", "though", "while", "unless", "until", "provided", "that", "assuming", "that", "case", "lest", "than", "rather", "than", "whether", "much", "whereas", "after", "long", "soon", "before", "time", "that", "once", "since", "till", "until", "when", "whenever", "while", "because", "since", "that", "order", "that", "what", "whatever", "which", "whichever", "whoever", "whom", "whomever", "whose", "though", "where", "wherever", "just", "both", "hardly", "when", "scarcely", "when", "either", "neither", "then", "what", "with", "whether", "only", "also", "sooner", "than", "rather", "than", "also", "besides", "furthermore", "likewise", "moreover", "Similar", "however", "nevertheless", "nonetheless", "still", "conversely", "instead", "otherwise", "rather", "Similar", "accordingly", "consequently", "hence", "meanwhile", "then", "therefore", "thus"];
+const MAX_LEN = 5;
+
 const body = document.querySelector("body");
 const outputWindow = document.querySelector("#json");
-// Async get data from dictionary lookup
-const getDictLookup = async (word, count) => {
-    const dictresp = await fetch(`${API_URL}?key=${API_KEY}&lang=en-en&text=${word}`);
-    const dictData = await dictresp.json();
-    const synonyms = dictData.def[0].tr.map(data => data.text);
-    const pos = dictData.def[0].pos;
-    return {
-        word,
-        output: {
-            count,
-            pos: dictData.def.length > 0 ? pos : null,
-            synonyms: dictData.def.length > 0 ? synonyms : null,
-        }
-    };
-}
 
-// fetch file url
+/* fetch file url */
 const getData = async () => {
-    // const response = await fetch(FILE_URL);
-    // const data = await response.text();
-    const data = `the uniformity of the middle west there was a certain monotony about pioneering in the northwest and on the middle border. as the long stretches of land were cleared or prepared for the plow, they were laid out like checkerboards into squares of forty, eighty, one hundred sixty, or more acres, each the seat of a homestead. there was a striking uniformity also about the endless succession of fertile fields spreading far and wide under the hot summer sun. no majestic mountains relieved the sweep of the prairie. few monuments of other races and antiquity were there to awaken curiosity about the region. no sonorous bells in old missions rang out the time of day. the chaffering red man bartering blankets and furs for powder and whisky had passed farther on. the population was made up of plain farmers and their families engaged in severe and unbroken labor, chopping down trees, draining fever-breeding swamps, breaking new ground, and planting from year to year the same rotation of crops. nearly all the settlers were of native american stock into whose frugal and industrious lives the later irish and german immigrants fitted, on the whole, with little friction. even the dutch oven fell before the cast-iron cooking stove. happiness and sorrow, despair and hope were there, but all encompassed by the heavy tedium of prosaic sameness.`;
 
-    // Create word array from the data
-    const wordsData = data.toLowerCase().split(/[''-.,\s]/);
+    try {
 
-    // Sanitise data
-    const wordsArray = sanitizeData(wordsData);
+        /* Step 1: start the fetch and obtain a reader */
+        let response = await fetch('https://cors-anywhere.herokuapp.com/' + FILE_URL);
 
-    // console.log(wordsArray);
-    // return
+        const reader = response.body.getReader();
 
-    const requests = wordsArray.map(word => getDictLookup(word.name, word.total).then(a => a));
-    body.classList.add('loading');
-    Promise
-        .all(requests)
-        .then(data => {
-            body.classList.remove('loading');
-            outputWindow.textContent = JSON.stringify(data, undefined, 4);
-        })
-        .catch(err => console.log(err));
+        /* Step 3: read the data */
+        let receivedLength = 0;
+        let chunks = [];
+        while (true) {
+            const { done, value } = await reader.read();
+
+            if (done) break;
+
+            chunks.push(value);
+            receivedLength += value.length;
+        }
+
+        /* Step 4: concatenate chunks into single Uint8Array */
+        let chunksAll = new Uint8Array(receivedLength);
+        let position = 0;
+        for (let chunk of chunks) {
+            chunksAll.set(chunk, position);
+            position += chunk.length;
+        }
+
+        /* Step 5: decode into a string */
+        const data = new TextDecoder("utf-8").decode(chunksAll);
+
+        /* Create word array from the data */
+        const wordsData = data.toLowerCase().split(/[''-.,\s]/);
+
+        /* Sanitise data */
+        const wordsArray = await sanitizeData(wordsData);
+
+        const requests = wordsArray.map(word => getSynonyms(word.name, word.total));
+        body.classList.add('loading');
+        Promise
+            .all(requests)
+            .then(data => {
+                body.classList.remove('loading');
+                outputWindow.textContent = JSON.stringify(data, undefined, 4);
+                console.log(data);
+            })
+            .catch(err => console.log(err));
+
+    } catch (error) {
+        console.error(error)
+    }
+
 }
 
 console.log(getData());
 
-function sanitizeData(data) {
-    // Count the occurence of each word and store in array as value => key pair
+/* Async get data from dictionary lookup */
+async function getSynonyms(word, count) {
+    const resp = await fetch(`${API_URL}?key=${API_KEY}&lang=en-en&text=${word}`);
+    const data = await resp.json();
+
+    const synonyms = data.def.length > 0 ? data.def[0].tr.map(data => data.text) : null;
+    const pos = data.def.length > 0 ? data.def[0].pos : null;
+    return {
+        word,
+        output: {
+            count,
+            pos: pos,
+            synonyms: synonyms,
+        }
+    };
+}
+
+function sanitizeData(words) {
+    /* Count the occurence of each word and store in array as value => key pair */
     const wordsMap = {};
-    data.forEach(word => {
-        // Add word to wordsMap as key
+    words.forEach(word => {
+        /* Add word to wordsMap as key */
         word = word.trim();
         if (wordsMap.hasOwnProperty(word)) {
             wordsMap[word]++;
@@ -63,10 +95,10 @@ function sanitizeData(data) {
         }
     });
 
-    // Create a words map to store the count of each word in wordsArray
-    let dataArray = [];
-    dataArray = Object.keys(wordsMap).map((word) => {
-        if (word.length > 3 && !skippedWords.includes(word)) {
+    /* Create a words map to store the count of each word in wordsArray */
+    let wordsArray = [];
+    wordsArray = Object.keys(wordsMap).map((word) => {
+        if (word.length >= MAX_LEN && !skippedWords.includes(word)) {
             return {
                 name: word,
                 total: wordsMap[word]
@@ -74,10 +106,10 @@ function sanitizeData(data) {
         }
     });
 
-    // sort by total count in descending order
-    dataArray.sort((a, b) => b.total - a.total);
+    /* sort by total count in descending order */
+    wordsArray.sort((a, b) => b.total - a.total);
 
-    dataArray = dataArray.slice(0, 10);
+    wordsArray = wordsArray.slice(0, 10);
 
-    return dataArray;
+    return wordsArray;
 }
